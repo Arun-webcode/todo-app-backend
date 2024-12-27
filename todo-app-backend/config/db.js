c2e93ed5bf43b561f3236d840db6acf3a585a61b
Arun <vikrantkumarprajapati4@gmail.com>
2024-12-27 23:50:01 +0530

    server file updated

--- todo-app-backend/config/db.js
+++ todo-app-backend/config/db.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error(err.message);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
-module.exports = connectDB;
+
+export default connectDB;
--- todo-app-backend/server.js
+++ todo-app-backend/server.js
@@ -1,26 +1,39 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const connectDB = require("./config/db");
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
 
-// Load environment variables
-dotenv.config();
-
-// Connect to MongoDB
-connectDB();
+dotenv.config(); // Load environment variables
 
 const app = express();
 
 // Middleware
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOption = {
+    origin: "http://localhost:8100",
+    credentials: true,
+};
+app.use(cors(corsOption));
 
 // Routes
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/tasks", require("./routes/taskRoutes"));
-app.use("/api/user", require("./routes/userRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/tasks", require("./routes/taskRoutes"));
+app.use("/api/v1/user", require("./routes/userRoutes"));
 
+// Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
+});
+
+// Server setup
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, async () => {
+    await connectDB();
+    console.log(`Server running at port ${PORT}`);
+});
