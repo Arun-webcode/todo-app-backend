c3b8d63733ce3e0e1d6b5fc8a3a879f958e12915
Arun <vikrantkumarprajapati4@gmail.com>
2024-12-29 23:12:08 +0530

    server setup with mongo connection

--- todo-app-backend/server.js
+++ todo-app-backend/server.js
@@ -1,10 +1,10 @@
-import express from "express";
 import cookieParser from "cookie-parser";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./config/db";
+import connectDB from "./utils/db.js";
 
-dotenv.config(); // Load environment variables
+dotenv.config();
 
 const app = express();
 
@@ -20,20 +20,19 @@ const corsOption = {
 app.use(cors(corsOption));
 
 // Routes
-app.use("/api/v1/auth", require("./routes/authRoutes"));
-app.use("/api/v1/tasks", require("./routes/taskRoutes"));
-app.use("/api/v1/user", require("./routes/userRoutes"));
+// app.use("/api/v1/auth", require("./routes/authRoutes"));
+// app.use("/api/v1/tasks", require("./routes/taskRoutes"));
+// app.use("/api/v1/user", require("./routes/userRoutes"));
 
 // Error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("Something went wrong!");
-});
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send("Something went wrong!");
+// });
 
-// Server setup
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, async () => {
-    await connectDB();
+app.listen(PORT,() => {
+    connectDB();
     console.log(`Server running at port ${PORT}`);
 });
